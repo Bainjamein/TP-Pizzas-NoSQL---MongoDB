7a7c4ebb5c8a29c604da443994c6feb299122446
quentinsvn <quentin.savean@gmail.com>
2024-02-15 14:12:54 +0100

    Réponses étape 3

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
-Benjamin Perchepied - Quentin Savean MAALSI23
+# TP Pizza - NoSQL : MongoDB
+## MAALSI23
+### Benjamin Perchepied - Quentin Savéan
 
-Commande pour run : sudo docker-compose up --build
+Commande pour run : __docker-compose up --build__
 
-Etape 3 :
+**Etape 3 :**
 
+a. Quel est le montant total des commandes de pizzas (tous formats confondus) ?
+
+> 134 €
+
+b. Combien de pizzas ont été commandées (toutes recettes et formats
+confondus) ?
+
+> 8 commandes
+
+c. Combien de pizzas "Vegan" ont été commandées ?
+
+> 2 commandes
+
+d. Combien de pizzas ont été commandées en format "large" ?
+
+> 2 commandes
+
+e. Quelle recette de pizza a été la plus vendue ?
+
+> Cheese
+
+f. Quel format de pizza a été le plus vendu ?
+
+> Small
+
+g. Quelle recette de pizza a rapporté le plus de revenus ?
+
+> Pepperoni
--- app-python/app.py
+++ app-python/app.py
@@ -9,7 +9,7 @@ class OrderService:
     def getOrders(self):
         return list(self.orders_collection.find())
 
-    def getOrdersByName(self, name):
+    def getOrdersByPizza(self, name):
         return list(self.orders_collection.find({"name": name}))
 
     def getOrdersBySize(self, size):
@@ -17,7 +17,18 @@ class OrderService:
 
     def getOrdersByCriteria(self, criteria):
         return list(self.orders_collection.find(criteria))
+    
+    def getTotalPriceOrders(self):
+        return list(self.orders_collection.aggregate([{"$group": {"_id": None, "total": {"$sum": "$price"}}}]))[0]["total"]
 
+    def getPizzaMostOrdered(self):
+        return list(self.orders_collection.aggregate([{"$group": {"_id": "$name", "total": {"$sum": 1}}}, {"$sort": {"total": -1}}]))[0]["_id"]
+
+    def getSizePizzaMostOrdered(self):
+        return list(self.orders_collection.aggregate([{"$group": {"_id": "$size", "total": {"$sum": 1}}}, {"$sort": {"total": -1}}]))[0]["_id"]
+
+    def getPizzaMostIncome(self):
+        return list(self.orders_collection.aggregate([{"$group": {"_id": "$name", "total": {"$sum": "$price"}}}, {"$sort": {"total": -1}}]))[0]["_id"]
 if __name__ == "__main__":
 
     client = MongoClient('mongodb://mongodb:27017/')
@@ -28,7 +39,7 @@ if __name__ == "__main__":
     print(order_service.getOrders())
 
     print("Commandes pour la pizza Pepperoni:")
-    print(order_service.getOrdersByName("Pepperoni"))
+    print(order_service.getOrdersByPizza("Pepperoni"))
 
     print("Commandes de grande taille:")
     print(order_service.getOrdersBySize("large"))
@@ -36,3 +47,21 @@ if __name__ == "__main__":
     print("Commandes de pizza Margherita de grande taille:")
     criteria = {"name": "Pepperoni", "size": "large"}
     print(order_service.getOrdersByCriteria(criteria))
+
+    print("Prix total des commandes:")
+    print(order_service.getTotalPriceOrders())
+
+    print("Nombre de pizzas Vegan commandées:")
+    print(len(order_service.getOrdersByPizza("Vegan")))
+
+    print("Nombre de commandes de grandes tailles:")
+    print(len(order_service.getOrdersBySize("large")))
+
+    print("Pizza la plus commandée:")
+    print(order_service.getPizzaMostOrdered())
+
+    print("Format de pizza la plus commandée:")
+    print(order_service.getSizePizzaMostOrdered())
+
+    print("Recette de pizza avec le plus de revenus:")
+    print(order_service.getPizzaMostIncome())
